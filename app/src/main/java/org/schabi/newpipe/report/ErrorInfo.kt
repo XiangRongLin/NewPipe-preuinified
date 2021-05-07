caaceb979459ad49ead9cbdf1b3ca7100d2feed5
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2021-05-07 11:45:29 +0200

    follow up fix to cherry pick

--- app/src/main/java/org/schabi/newpipe/report/ErrorInfo.kt
+++ app/src/main/java/org/schabi/newpipe/report/ErrorInfo.kt
@@ -5,19 +5,19 @@ import androidx.annotation.StringRes
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class ErrorInfo private constructor(
-        val userAction: UserAction?,
-        val serviceName: String,
-        val request: String,
-        @field:StringRes @param:StringRes val message: Int
+class ErrorInfo(
+    val userAction: UserAction?,
+    val serviceName: String,
+    val request: String,
+    @field:StringRes @param:StringRes val message: Int
 ) : Parcelable {
     companion object {
         @JvmStatic
         fun make(
-                userAction: UserAction?,
-                serviceName: String,
-                request: String,
-                @StringRes message: Int
+            userAction: UserAction?,
+            serviceName: String,
+            request: String,
+            @StringRes message: Int
         ) = ErrorInfo(userAction, serviceName, request, message)
     }
 }
